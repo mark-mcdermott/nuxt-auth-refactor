62ab283ae8f97cac8f2f80da3e968e98e8570aba
Mark McDermott <mark@markmcdermott.io>
2022-08-10 21:27:53 -0500

    refactor seeds

--- backend/db/schema.rb
+++ backend/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_08_11_021053) do
+ActiveRecord::Schema[7.0].define(version: 2022_08_11_022709) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -24,6 +24,14 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_11_021053) do
     t.integer "account_type_id"
     t.integer "user_id"
     t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "assets", force: :cascade do |t|
+    t.string "name"
+    t.integer "value"
+    t.integer "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
@@ -71,6 +79,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_11_021053) do
     t.string "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "account_id"
   end
 
   create_table "users", force: :cascade do |t|
--- backend/db/seeds/1_user_seed.rb
+++
@@ -1,4 +0,0 @@
-User.create(:email => 'mark@markmcdermott.io', :password => 'password')
-User.create(:email => 'admin@example.com', :password => 'password')
-User.create(:email => 'demo1@example.com', :password => 'password')
-User.create(:email => 'demo2@example.com', :password => 'password')
